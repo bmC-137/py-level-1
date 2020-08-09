54fff51ec5056992577fdd06521c4025ff9cfc08
bmC-137 <64267542+bmC-137@users.noreply.github.com>
2020-08-09 16:41:45 +0300

    c

--- base_2/most_frequent_element_in_list.py
+++ base_2/most_frequent_element_in_list.py
@@ -1,27 +1,27 @@
 import random
 from random import randrange
 from pprint import pprint
-from typing import Dict
 
-c= []
+c = []
 b = [c.append(random.randrange(20)) for i in range(100)]
 
-
 print('Test.1 with counter')
+
+
 def most_frequent(List):
-    counter = 0
-    num = List[0]
-    print('Printing num: ', num)
+	counter = 0
+	num = List[0]
+	print('Printing num: ', num)
 
-    for i in List:
-        print('element:',i, end=' ')
-        curr_frequency = List.count(i)
-        print('frequency:',curr_frequency, end='\n')
-        if (curr_frequency > counter):
-            counter = curr_frequency
-            num = i
+	for i in List:
+		print('element:', i, end=' ')
+		curr_frequency = List.count(i)
+		print('frequency:', curr_frequency, end='\n')
+		if (curr_frequency > counter):
+			counter = curr_frequency
+			num = i
 
-    return num
+	return num
 
 
 a = most_frequent(c)
@@ -32,9 +32,9 @@ print('Most is: ', a, end='\n\n\n\n')
 ###
 print('Test.2 with dictionary and \"count\" method with max')
 
-d: Dict[int, int] = {}
+d = {}
 for i in c:
-    d[i] = c.count(i)
+	d[i] = c.count(i)
 # adding some sort to dictionary.
 print(dict(sorted(d.items())))
 print('Max Value is: ', max(d.keys(), key=d.get), end='\n\n\n\n')
@@ -48,11 +48,24 @@ d.get => gets the value associated with that key
 ##
 ##
 
-
 print('Test.3 with set and list and max')
-
 # def most_frequent1(List):
 #     return max(set(List), key=List.count)
-
 print('Max Value is: ', max(set(c), key=c.count), end='\n\n\n\n')
-# print(most_frequent(c),)
+##
+##
+##
+##
+
+print('Test.4 with counter')
+from collections import Counter
+
+
+def most_freq(l_):
+	occurence_count = Counter(l_)
+	return occurence_count.most_common(1)
+
+
+b_ = most_freq(c)
+for key, value in b_:
+	print('Most Freq:', key, 'wich repeated:', value)
